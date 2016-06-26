9be39ace5482f42011d38e769b9a0d55829c23b3
sorrina <1981maariaa@gmail.com>
2016-06-26 21:44:02 +0200

    Bugfix

--- drips.php
+++ drips.php
@@ -5,30 +5,32 @@ use Drips\Config\Config;
 use Drips\Database\DB;
 
 if(class_exists('Drips\App')){
-    $app = App::getInstance();
+    App::on('create', function(){
+        $app = App::getInstance();
 
-    $type = Config::get('database_type', 'mysql');
-    $host = Config::get('database_host', 'localhost');
-    $database = Config::get('database_name', 'drips');
-    $username = Config::get('database_username', 'root');
-    $password = Config::get('database_password', 'root');
-    $port = Config::get('database_port', 3306);
-    $charset = Config::get('database_charset', 'utf-8');
+        $type = Config::get('database_type', 'mysql');
+        $host = Config::get('database_host', 'localhost');
+        $database = Config::get('database_name', 'drips');
+        $username = Config::get('database_username', 'root');
+        $password = Config::get('database_password', 'root');
+        $port = Config::get('database_port', 3306);
+        $charset = Config::get('database_charset', 'utf-8');
 
-    $config = array(
-        'database_type' => $type
-    );
+        $config = array(
+            'database_type' => $type
+        );
 
-    if($type == 'sqlite'){
-        $config['database_file'] = $host;
-    } else {
-        $config['server'] = $host;
-        $config['database_name'] = $database;
-        $config['username'] = $username;
-        $config['password'] = $password;
-        $config['port'] = $port;
-        $config['charset'] = $charset;
-    }
+        if($type == 'sqlite'){
+            $config['database_file'] = $host;
+        } else {
+            $config['server'] = $host;
+            $config['database_name'] = $database;
+            $config['username'] = $username;
+            $config['password'] = $password;
+            $config['port'] = $port;
+            $config['charset'] = $charset;
+        }
 
-    $app->db = new DB($config);
+        $app->db = new DB($config);
+    });
 }
